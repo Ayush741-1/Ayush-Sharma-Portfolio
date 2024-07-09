b0a0579c04005a7d97f656d687e94e61e9f716c9
Ayush Sharma <114041059+Ayush741-1@users.noreply.github.com>
2024-07-09 19:05:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// text animation typed.js
+let typed = new Typed('#element', {
+    strings: ['Frontend Deveoper', 'Software Developer', 'Application Developer'],
+    typeSpeed: 50,
+  });
+
+// Send Mail
+let sendMail = ()=>{
+  let params = {
+    from_name : document.getElementById('fullName').value,
+    email_id : document.getElementById('email').value,
+    message : document.getElementById('message').value
+  }
+  console.log(from_name.value)
+  emailjs.send("service_5ybrn9i","template_x77vbbs", params).then(()=>{
+    alert("Email Sent Successfully!")
+  })
+  // Clearing input fields
+  document.getElementById("fullName").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
+
+// custom modal
+document.addEventListener("DOMContentLoaded", function() {
+  var aboutLink = document.getElementById("aboutLink");
+  var aboutModal = new bootstrap.Modal(document.getElementById("aboutModal"));
+  
+  aboutLink.addEventListener("click", function(e) {
+    e.preventDefault();
+    aboutModal.show();
+  });
+});
